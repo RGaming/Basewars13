c444ef7bb1142a59b9f0b9ac3bad9b1ae9e01d6a
Rudy Gamberini <RudyGamberini@gmail.com>
2012-11-14 15:23:19 -0500

    Added necacary gamemod files
    Gamemode now runs

---
+++ gamemode/init.lua
@@ -0,0 +1,20 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "HUD.lua" )
+
+include( "shared.lua" )
+--include( "database.lua")
+
+function GM:PlayerSpawn( ply )
+      ply:SetGravity( 0.75 )
+      ply:SetMaxHealth( 100, true )
+      ply:SetRunSpeed( 400 )
+      ply:SetWalkSpeed( 350 )
+	  ply:Give( "weapon_physcannon" )
+      ply:Give( "weapon_physgun" )
+      ply:Give( "gmod_tool" )
+end
+
+function GM:PlayerInitialSpawn( ply )
+    
+end
