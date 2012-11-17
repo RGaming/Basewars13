8851bfe74a46124e1f5aefb13d50b206c3baf1b5
Nick Gamberini <nickelpro@gmail.com>
2012-11-17 00:11:29 -0500

    Made a thing
    For you Rudy

--- gamemode/database.lua
+++ gamemode/database.lua
@@ -1,4 +1,15 @@
 if not sql.TableExists( "Basewars13" )
-	sql.Query( "CREATE TABLE IF NOT EXISTS Basewars13 (Money TEXT, SteamID TEXT)" )
+	sql.Query( "CREATE TABLE IF NOT EXISTS Basewars13 (SteamID TEXT, Money REAL);" )
 end
 
+function PlayerExists( SteamID )
+	SqlString = "SELECT EXISTS(SELECT 1 FROM Basewars13 WHERE SteamID = %s);"
+	if sql.Query(string.format(SqlString, sql.SQLStr(SteamID))) then
+		return true
+	else
+		return false
+end
+
+function CreatePlayer( SteamID )
+	SqlString = "SELECT EXISTS(SELECT 1 FROM Basewars13 WHERE SteamID = %s);"
+end
