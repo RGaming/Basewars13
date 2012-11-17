5a13d2fb787415d0c9e099de15233ceee6907d89
Nick Gamberini <nickelpro@gmail.com>
2012-11-17 13:36:54 -0500

    Added concommand
    Will most likely break

--- gamemode/concommand.lua
+++ gamemode/concommand.lua
@@ -1,4 +1,4 @@
-function buyprinter( ply )
+function buyprint_bronze( ply )
 	local printer = ents.Create("money_printer_bronze")
 	if CanAfford(ply, printer.Price) then
 		RemoveMoney(ply, printer.Price)
@@ -8,3 +8,4 @@ function buyprinter( ply )
 	else
 		printer:Remove()
 end
+concommand.Add("buyprint_bronze", buyprint_bronze(ply))
