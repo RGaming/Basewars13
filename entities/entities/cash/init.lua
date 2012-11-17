99df127a849d2ca0a980c85dc572b6f78e140ab4
Rudy Gamberini <Don'tEmailMe@Stahp.com>
2012-11-16 23:03:24 -0800

    Added cash and printers

---
+++ entities/entities/cash/init.lua
@@ -0,0 +1,24 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include('shared.lua')
+
+function ENT:Initialize()
+	-- Boiler plate
+	self.Entity:SetModel( "models/Combine_Helicopter/helicopter_bomb01.mdl" )
+	self.Entity:PhysicsInit(SOLID_VPHYSICS)
+	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
+	self.Entity:SetSolid(SOLID_VPHYSICS)
+	local phys = self:GetPhysicsObject()
+	if (phys:IsValid()) then
+		phys:Wake()
+	end
+	--
+end
+
+function ENT:Use( activator, caller )
+	AddMoney(activator, self.worth)
+end
+
+function ENT:SetWorth( worth )
+	self.worth = worth
+end
---
+++ entities/entities/money_printer_bronze/shared.lua
@@ -0,0 +1,10 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ 
+ENT.PrintName		= "Bronze Printer"
+ENT.Author			= "[RG]Uggleking"
+ENT.Contact			= "Don't"
+ENT.Purpose			= "Make it rain baby"
+ENT.Instructions	= "Use with care. Always handle with gloves."
+ENT.Payout			= 1000
+ENT.Price 			= 3200
