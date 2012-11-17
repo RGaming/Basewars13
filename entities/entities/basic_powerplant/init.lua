0ba4094f24ac99adfdea914b0e959b7e15b99124
Rudy Gamberini <Don'tEmailMe@Stahp.com>
2012-11-17 00:25:53 -0800

    Started on powerplant

---
+++ entities/entities/basic_powerplant/init.lua
@@ -0,0 +1,25 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include('shared.lua')
+
+function ENT:Initialize()
+	-- Boiler plate
+	self.Entity:SetModel( "models/props/cs_assault/Money.mdl" )
+	self.Entity:PhysicsInit(SOLID_VPHYSICS)
+	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
+	self.Entity:SetSolid(SOLID_VPHYSICS)
+	local phys = self:GetPhysicsObject()
+	if (phys:IsValid()) then
+		phys:Wake()
+	end
+	--
+end
+
+function ENT:Use( activator, caller )
+	AddMoney(activator, self.worth)
+	self:Remove()
+end
+
+function ENT:SetWorth( worth )
+	self.worth = worth
+end
--- entities/entities/basic_powerplant/shared.lua
+++ entities/entities/basic_powerplant/shared.lua
@@ -1,12 +1,9 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
  
-ENT.PrintName		= "Bronze Printer"
+ENT.PrintName		= "Cash"
 ENT.Author			= "[RG]Uggleking"
 ENT.Contact			= "Don't"
 ENT.Purpose			= "Make it rain baby"
 ENT.Instructions	= "Use with care. Always handle with gloves."
-ENT.Payout			= 160
-ENT.Delay 			= 35
-ENT.Price 			= 3200
-ENT.PowerUsage		= 1
+ENT.worth			= 0
