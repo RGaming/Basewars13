c296664662ae3a432154bccecd99fd6c95a40204
Rudy Gamberini <Don'tEmailMe@Stahp.com>
2012-11-18 15:35:36 -0800

    Nothing of note

--- entities/entities/dispenser/init.lua
+++ entities/entities/dispenser/init.lua
@@ -4,7 +4,7 @@ include('shared.lua')
 
 function ENT:Initialize()
 	-- Boiler plate
-	self.Entity:SetModel( "models/props_lab/reciever_cart.mdl" )
+	self.Entity:SetModel( self.Model )
 	self.Entity:PhysicsInit(SOLID_VPHYSICS)
 	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
 	self.Entity:SetSolid(SOLID_VPHYSICS)
--- entities/entities/dispenser/shared.lua
+++ entities/entities/dispenser/shared.lua
@@ -1,9 +1,11 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
  
-ENT.PrintName		= "Cash"
+ENT.PrintName		= "Dispenser"
 ENT.Author			= "[RG]Uggleking"
 ENT.Contact			= "Don't"
 ENT.Purpose			= "Make it rain baby"
 ENT.Instructions	= "Use with care. Always handle with gloves."
 ENT.Price 			= "1500"
+ENT.Model 			= "models/props_lab/reciever_cart.mdl"
+ENT.Ent_Name 		= "dispenser"
